2236db61701150e0782116b36cb39143e1c8e9d4
dangus21 <angusmiguel21@gmail.com>
2020-09-18 11:19:40 +0100

    Rollup Css config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
-import scss from 'rollup-plugin-scss'
+import postcss from "rollup-plugin-postcss";
 
 import packageJson from "./package.json";
 
@@ -10,10 +10,23 @@ export default {
     input: "./src/index.ts",
     output: [
         {
+            name: 'mut',
             file: packageJson.main,
-            format: "cjs",
-            sourcemap: true
+            format: "umd",
+            sourcemap: true,
+            globals: { react: 'React' },
         },
     ],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), scss()]
+    plugins: [
+        peerDepsExternal(),
+        typescript(),
+        resolve(),
+        commonjs(),
+        postcss({
+            extract: false,
+            modules: true,
+            use: ['sass'],
+        }),
+    ],
+    external: ['react', 'react-dom'],
 };
