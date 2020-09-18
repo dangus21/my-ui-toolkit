ffbba1975194219cfa875ec18a86ce1efab0bef0
dangus21 <angusmiguel21@gmail.com>
2020-09-18 11:29:54 +0100

    Removed webpack

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import autoprefixer from 'autoprefixer'
 
 import packageJson from "./package.json";
 
@@ -23,8 +24,10 @@ export default {
         resolve(),
         commonjs(),
         postcss({
+            extract: true,
             extract: false,
             modules: true,
+            writeDefinitions: true,
             use: ['sass'],
         }),
     ],
