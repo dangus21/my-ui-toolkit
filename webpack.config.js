c6105a25a6aa7c5619a00e3aecf7ba440ed1f393
dangus21 <angusmiguel21@gmail.com>
2020-10-04 22:50:46 +0100

    Changed target lib

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
         library: 'mut',
-        libraryTarget: 'commonjs'
+        libraryTarget: 'umd'
     },
     plugins: [new CleanWebpackPlugin()],
     module: {
